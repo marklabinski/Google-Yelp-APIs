3ee42567f09ba06a6f41995483824b6494d3e686
Mark Labinski <40125783+marklabinski@users.noreply.github.com>
2018-07-17 13:31:25 -0500

    Add files via upload

---
+++ GoogleAPI.py
@@ -0,0 +1,18 @@
+## Google API Data
+
+# File Information
+
+### Description 
+Automated python script to pull data from Google Nearby Search API.
+
+### Functions
+# GoogleRequest__ : Grabs and parses JSON data from Google API
+# GoogleSearch__ : Performs Google API search using GoogleRequest
+
+
+
+For API documentation, see:
+https://developers.google.com/places/web-service/search
+
+
+Note: 
